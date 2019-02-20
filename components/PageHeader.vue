8c161dca1fd7040c17eef15da038bab4815e1147
speery <stephen.peery@cbsinteractive.com>
2019-02-20 16:59:42 -0500

    initial commit

---
+++ components/PageHeader.vue
@@ -0,0 +1,7 @@
+<template>
+  <header class="text-center">
+    <h1>
+        <a class="no-underline text-grey-darkest" href="/">smplman Project Site</a>
+    </h1>
+  </header>
+</template>
---
+++ nuxt.config.js
@@ -0,0 +1,131 @@
+require('dotenv').config()
+const PurgecssPlugin = require('purgecss-webpack-plugin')
+const glob = require('glob-all')
+const path = require('path')
+import axios from 'axios'
+
+class TailwindExtractor {
+  static extract(content) {
+    return content.match(/[A-z0-9-:\/]+/g) || [];
+  }
+}
+
+module.exports = {
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: 'smplman',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'smplman project website' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  /*
+  ** Modules
+  */
+  modules: [
+    '@nuxtjs/axios',
+  ],
+  /*
+  ** Plugins
+  */
+  plugins: [
+    '~/plugins/filters.js'
+  ],
+  /*
+  ** Axios
+  */
+  axios: {
+    // proxyHeaders: false
+  },
+  /*
+  ** Customize the progress bar color
+  */
+  loading: { color: '#3B8070' },
+  /*
+  ** Global CSS
+  */
+  css: [
+    '@/assets/css/main.css'
+  ],
+  /*
+  ** Generate Routes
+  */
+  generate: {
+    routes: async () => {
+      let { data } = await axios.post(process.env.POSTS_URL,
+      JSON.stringify({
+          filter: { published: true },
+          sort: {_created:-1},
+          populate: 1
+        }),
+      {
+        headers: { 'Content-Type': 'application/json' }
+      })
+      return data.entries.map((post) => {
+        return {
+          route: post.title_slug,
+          payload: post
+        }
+      })
+    }
+  },
+  /*
+  ** Build configuration
+  */
+  build: {
+    extractCSS: true,
+    /*
+    ** Run ESLint on save
+    */
+    extend (config, { isDev, isClient }) {
+      if (isDev && isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/,
+        })
+      }
+
+      if (!isDev) {
+        // Remove unused CSS using purgecss. See https://github.com/FullHuman/purgecss
+        // for more information about purgecss.
+        config.plugins.push(
+          new PurgecssPlugin({
+            // Specify the locations of any files you want to scan for class names.
+            paths: glob.sync([
+              path.join(__dirname, './pages/**/*.vue'),
+              path.join(__dirname, './layouts/**/*.vue'),
+              path.join(__dirname, './components/**/*.vue')
+            ]),
+            extractors: [
+              {
+                extractor: TailwindExtractor,
+                // Specify the file extensions to include when scanning for
+                // class names.
+                extensions: ["html", "vue"]
+              }
+            ],
+            whitelist: [
+              "html",
+              "body",
+              "ul",
+              "ol",
+              "pre",
+              "code",
+              "blockquote"
+            ],
+            whitelistPatterns: [/\bhljs\S*/]
+          })
+        )
+      }
+    }
+  }
+}
+
