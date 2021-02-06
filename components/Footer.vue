0946d07e635eb0edbd1e9182ac100c86da4e0fa7
smp4488 <smp4488@aol.com>
2021-02-06 18:05:04 -0500

    adding GA and btc donate address

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,7 +1,8 @@
 <template>
   <footer class="flex justify-center my-4">
     <div class="text-gray-900 text-sm">
-      A static blog built with Nuxt.js, Tailwindcss and Cockpit.
+      <p>A static blog built with Nuxt.js, Tailwindcss and Cockpit.</p>
+      <p>Donate BTC : 1Bbzjkxp5DdsBT1Xwv5y6uTqtW8Wy4F2Rh</p>
     </div>
   </footer>
 </template>
